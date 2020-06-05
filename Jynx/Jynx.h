f509aef425d883e8792c69f1f885f9fa2eea236e
Eric T Cormack <bobryuu@gmail.com>
2020-06-05 13:21:01 -0500

    Adding base file structure

---
+++ Jynx/Jynx.h
@@ -0,0 +1,19 @@
+//
+//  Jynx.h
+//  Jynx
+//
+//  Created by Eric T Cormack on 6/5/20.
+//  Copyright © 2020 Eric T Cormack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jynx.
+FOUNDATION_EXPORT double JynxVersionNumber;
+
+//! Project version string for Jynx.
+FOUNDATION_EXPORT const unsigned char JynxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jynx/PublicHeader.h>
+
+
--- JynxCLI/main.swift
+++ JynxCLI/main.swift
@@ -7,6 +7,6 @@
 //
 
 import Foundation
+import Jynx
 
 print("Hello, World!")
-
