e1a8410459681cce92e607c97bed7ef36eb26f62
Eric T Cormack <bobryuu@gmail.com>
2020-06-05 13:10:40 -0500

    Initial Commit

---
+++ JynxCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JynxCLI
+//
+//  Created by Eric T Cormack on 6/5/20.
+//  Copyright © 2020 Eric T Cormack. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
